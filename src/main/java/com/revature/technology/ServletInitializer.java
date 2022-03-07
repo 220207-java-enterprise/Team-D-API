01704629e88a39ad9906bca115862ed231ec1833
Adam Lyn <adam273@revature.net>
2022-03-07 16:52:29 -0500

    Trying for a merge conflict baby

--- src/main/java/com/revature/technology/ServletInitializer.java
+++ src/main/java/com/revature/technology/ServletInitializer.java
@@ -3,8 +3,12 @@ package com.revature.technology;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+public class ServletInitialize extends SpringBootServletInitializer {
 
+	public static void main(String[] args) {
+		System.out.println("Yeah boyyy");
+	}
+	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(TechnologyApplication.class);
