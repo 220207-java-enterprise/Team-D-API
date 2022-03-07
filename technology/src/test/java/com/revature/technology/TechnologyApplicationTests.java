f37ae9860faea5854107873171c41d3705518b63
Ryan Odeneal <ryan063@revature.net>
2022-03-07 14:07:04 -0600

    Project initialization

---
+++ technology/src/test/java/com/revature/technology/TechnologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.technology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
