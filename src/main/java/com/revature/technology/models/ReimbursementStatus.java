620cd2c4875440c2e3f54c8bb6c8f7cf2dc3e0e6
zhenying <a664330348@gmail.com>
2022-03-09 10:30:04 -0500

    update Reimbursement Status and Type

--- src/main/java/com/revature/technology/models/ReimbursementStatus.java
+++ src/main/java/com/revature/technology/models/ReimbursementStatus.java
@@ -1,6 +1,42 @@
 package com.revature.technology.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ers_reimbursement_statuses")
 public class ReimbursementStatus {
     //It needs to be called status_id for Reimbursement
+
+    @Id
     private String status_id;
+
+    @Column(unique = true)
+    private String status;
+
+    public String getStatus_id() {
+        return status_id;
+    }
+
+    public void setStatus_id(String status_id) {
+        this.status_id = status_id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "ErsReimbStatus{" +
+                "status_id='" + status_id + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/revature/technology/models/ReimbursementType.java
+++ src/main/java/com/revature/technology/models/ReimbursementType.java
@@ -1,6 +1,42 @@
 package com.revature.technology.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "ers_reimbursement_types")
 public class ReimbursementType {
     //It needs to be called type_id for Reimbursement
+    @Id
     private String type_id;
+
+    @Column(unique = true)
+    private String type;
+
+    public String getType_id() {
+        return type_id;
+    }
+
+    public void setType_id(String type_id) {
+        this.type_id = type_id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "ErsReimbType{" +
+                "type_id='" + type_id + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
 }
