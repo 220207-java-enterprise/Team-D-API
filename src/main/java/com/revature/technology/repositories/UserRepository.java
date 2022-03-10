1b610d5e6500ed7d1721cee6affee6ffd848bb42
Ryan Odeneal <ryan063@revature.net>
2022-03-10 08:23:47 -0600

    Dummy data works now

--- src/main/java/com/revature/technology/repositories/UserRepository.java
+++ src/main/java/com/revature/technology/repositories/UserRepository.java
@@ -8,22 +8,22 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface UserRepository extends CrudRepository {
+public interface UserRepository extends CrudRepository<User, String> {
 
-    User getUserById(String id);
+   // User getUserById(String id);
 
-    User getUserByUsername(String username);
+    //User getUserByUsername(String username);
 
-    User getUserByEmail(String email);
+    //User getUserByEmail(String email);
 
-    User getUserByUsernameAndPassword(String username, String password);
+    //User getUserByUsernameAndPassword(String username, String password);
 
-    List<User> getAll();
+    //List<User> getAll();
 
-    List<User> getAllByActive(Boolean active);
+    //List<User> getAllByActive(Boolean active);
 
     //TODO correct command??
-    void update(User updatedObject);
+    //void update(User updatedObject);
 
 
 }
