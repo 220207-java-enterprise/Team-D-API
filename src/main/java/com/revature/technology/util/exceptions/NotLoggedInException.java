f9ce682c5a4b3f33102b13c28227380ca8925bb4
abhilekhx <lekh7adhikari@gmail.com>
2022-03-17 01:06:52 -0400

    implemented NotLoggedInException

--- src/main/java/com/revature/technology/util/exceptions/NotLoggedInException.java
+++ src/main/java/com/revature/technology/util/exceptions/NotLoggedInException.java
@@ -2,5 +2,7 @@ package com.revature.technology.util.exceptions;
 
 public class NotLoggedInException extends ResourceNotFoundException{
 
-    public NotLoggedInException(){super("Please login before making requests.");}
+    public NotLoggedInException(){super("Please login before making requests.");
+
+    }
 }
