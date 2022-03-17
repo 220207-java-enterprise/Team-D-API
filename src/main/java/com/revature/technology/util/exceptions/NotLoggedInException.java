f6036f5dfc6fd03d235ce6eefde380f2fbc0442e
Ryan Odeneal <RyanOde94@gmail.com>
2022-03-17 09:30:48 -0500

    Revert "Finance Managers can view Pending Reimbursements"

--- src/main/java/com/revature/technology/util/exceptions/NotLoggedInException.java
+++
@@ -1,6 +0,0 @@
-package com.revature.technology.util.exceptions;
-
-public class NotLoggedInException extends ResourceNotFoundException{
-
-    public NotLoggedInException(){super("Please login before making requests.");}
-}
