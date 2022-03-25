f6018ca4a95484aeba9b4853c9a8148ff77a6b3b
Adam Lyn <adam273@revature.net>
2022-03-25 02:03:02 -0400

    Final Commit, added swagger fixed exceptions

--- src/main/java/com/revature/technology/TechnologyApplication.java
+++ src/main/java/com/revature/technology/TechnologyApplication.java
@@ -3,6 +3,11 @@ package com.revature.technology;
 import com.revature.technology.models.Reimbursement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class TechnologyApplication {
@@ -11,4 +16,12 @@ public class TechnologyApplication {
 		SpringApplication.run(TechnologyApplication.class, args);
 	}
 
+	@Bean
+	public Docket api(){
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
 }
